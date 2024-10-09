a421416bd71605e0afbe441a807bb57f1a2a0656
Bernd Doser <bernd.doser@h-its.org>
2024-10-09 12:05:04 +0200

    use interactive python code to remember kubectl commands

---
+++ kubectl.py
@@ -0,0 +1,28 @@
+# %% [markdown]
+# # Kubernetes
+# Interactive shell commands to interact with a kubernetes cluster.
+
+# %% [markdown]
+# Define the namespace
+# %%
+namespace="flytesnacks-development"
+
+# List pods
+# %%
+!kubectl get pod -A -o wide
+
+# %%
+!kubectl get pod --namespace={namespace} -o wide
+
+
+## Print logs of a single pod
+# %%
+pod_name="apv7fpcvrhtjfs2mj6kn-n0-0"
+
+# %%
+kubectl logs --namespace={namespace} {pod_name}
+
+
+## Enter running pod
+# %%
+!kubectl exec --stdin --tty --namespace={namespace} {pod_name} -- /bin/bash
